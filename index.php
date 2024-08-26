31b96d506b1b27032ed7ccc31fb2cb1a60c683b3
danielhrenak <daniel.hrenak@gmail.com>
2024-08-26 11:33:30 +0200

    new design

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daniel Hrenák - Personal Homepage</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Daniel Hrenák</h1>
+        <p>I'm a game producer, team lead, PHP developer</p>
+    </header>
+    <section class="links">
+        <a href="cv.php">CV</a> |
+        <a href="projects.php">Projects</a> |
+        <a href="blog.php">Blog</a>
+    </section>
+    <section class="social-media">
+        <a href="https://www.linkedin.com/in/daniel-hrenak" target="_blank">
+            <img src="img/linkedin.png" width="60px" alt="LinkedIn">
+        </a>
+        <a href="https://github.com/danielhrenak" target="_blank">
+            <img src="img/github.png" width="60px" alt="GitHub">
+        </a>
+        <a href="https://www.goodreads.com/user/show/11116744-daniel-hrenak" target="_blank">
+            <img src="img/goodreads.png" width="60px" alt="Goodreads">
+        </a>
+    </section>
+</body>
+</html>
